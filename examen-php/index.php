874e440b72ecf6d59ccba29e67ecb3dbf72676d4
Adrián <arodrigueza@esliceu.net>
2025-12-12 17:02:46 +0100

    Delete examen-php directory
    a

--- examen-php/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . "/db.php";
-$pageTitle = "Inicio";
-require __DIR__ . "/includes/header.php";
-?>
-<h1>Futbolistas</h1>
-<?php
-require __DIR__ . "/includes/footer.php";
--- examen-php/login.php
+++
@@ -1,9 +0,0 @@
-<form action="read_user.php">Usuari
-    <input type="text" name="Usuario" />
-    <input type="POST" value="Llegir dades">
-</form>
-
-<form action="read_user.php">Usuari
-    <input type="text" name="Password" />
-    <input type="POST" value="Llegir dades">
-</form>
